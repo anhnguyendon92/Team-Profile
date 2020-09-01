2b09ad69b0839fd88de90ba2b98e3141a6fd9150
Anh Nguyen <anhnguyendon92@gmail.com>
2020-09-01 15:10:38 -0700

    updating

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Enginner");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRender");
+
+const employees = [];
+
+function initApp() {
+    startHtml();
+    addMember();
+}
+
+function addMember() {
+    inquirer.prompt([{
+        message: "Enter team member's name",
+        name: "name"
+    },
+    {
+        type:
+    }
+    }])
 
